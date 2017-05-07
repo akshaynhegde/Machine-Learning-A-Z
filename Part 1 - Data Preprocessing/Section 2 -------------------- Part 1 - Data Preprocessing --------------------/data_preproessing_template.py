15a7073ede2aecd0e0353c84a3342b55a0124c87
Akshay Hegde <akshaynhegde@gmail.com>
2017-05-07 14:02:20 +0530

    template code. removed the ones that aren’t required. available in the separate files

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preproessing_template.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data_preproessing_template.py
@@ -13,31 +13,13 @@ import matplotlib.pyplot
 dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:,3].values
-               
-#managing missing data
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer.fit(x[:,1:3])
-x[:, 1:3] = imputer.transform(x[:,1:3])
-
-
-#categorizing the data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelEncoder_x = LabelEncoder()
-x[:,0] = labelEncoder_x.fit_transform(x[:,0])
-
-# encode the country values
-oneHotEncoder = OneHotEncoder(categorical_features = [0])
-x = oneHotEncoder.fit_transform(x).toarray()
-labelEncoder_y = LabelEncoder()
-y = labelEncoder_y.fit_transform(y)
 
 #splitting data into training and test sets
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
 
-#feature scaling
-from sklearn.preprocessing import StandardScaler
-scaler_x = StandardScaler()
-x_train = scaler_x.fit_transform(x_train)
-x_test = scaler_x.transform(x_test)
+##feature scaling
+#from sklearn.preprocessing import StandardScaler
+#scaler_x = StandardScaler()
+#x_train = scaler_x.fit_transform(x_train)
+#x_test = scaler_x.transform(x_test)
